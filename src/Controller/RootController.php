dec7b86d48adc33aa8a1bc0995f869f3bf8040d4
Gianluca Moro <giangiammy@gmail.com>
2020-11-20 20:56:17 +0100

    initial commit

---
+++ src/Controller/RootController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RootController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function homeAction(LoggerInterface $appLogger)
+    {
+        $username = $this->get('security.token_storage')->getToken()->getUser()->getUsername();
+        $appLogger->info("IN: homeAction: username='" . $username . "'");
+
+        return $this->render('index.html.twig', [
+            'controller_name' => 'HomeController',
+            'username' => $username,
+        ]);
+    }
+
+}
